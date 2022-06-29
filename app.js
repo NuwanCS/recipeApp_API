b8148276c44fa84852f5bd34d3b661c5eef4c45f
Nuwan Senevirathne <smpg.nuwan@gmail.com>
2022-06-29 12:24:18 +0530

    settingup config files

--- app.js
+++ app.js
@@ -20,22 +20,13 @@ app.get('/api/recipes', (req, res)=>{
 
 app.get('/api/recipes/:id', (req, res)=>{
     const result = recipies.find(r => r.id === parseInt(req.params.id));
-    if(!result) res.status(404).send('Result not found!')
+    if(!result) return res.status(404).send('Result not found!')
     res.send(result)
 })
 
 app.post('/api/recipes', (req, res)=>{
-
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    })
-
-    const result = schema.validate(req.body)
-
-    if(result.error){
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    const result = validateRecipe(req.body);
+    if(result.error) return res.status(400).send(result.error.details[0].message);
 
     const recipe = {
         id: recipes.length +1,
@@ -45,6 +36,39 @@ app.post('/api/recipes', (req, res)=>{
     recipes.push(recipe);
     res.status(200).send(recipe);
 })
+
+app.put('/api/recipes/:id', (req, res)=>{
+    const searchResult = recipes.find(r=> r.id === parseInt(req.params.id));
+    if(!searchResult) return res.status(404).send('Invalid recipe ID')
+
+    
+    const {error} = validateRecipe(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    searchResult.name = req.body.name;
+    res.send(searchResult)
+
+})
  
+app.delete('/api/recipes/:id', (req, res)=>{
+    const recipe = recipes.find(r=> r.id === parseInt(req.params.id))
+    if(!recipe) return res.status(404).send('Invalid recipe ID');
+
+    const index = recipes.indexOf(recipe);
+    recipes.splice(index, 1);
+
+    res.send(recipe)
+
+
+})
+
+const validateRecipe = (recipe)=>{
+
+    const schema = Joi.object({
+        name:Joi.string().min(3).required()
+    })
+
+    return schema.validate(recipe)
+}
 const port = process.env.PORT || 3000
 app.listen(port, ()=> console.log(`Listening on Port ${port}..`));
